840e9e536daf19ebde6329b61403c3253711f273
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-15 21:19:59 +0600

    Playing French sounds

--- app/src/main/java/com/example/french_teacher_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/french_teacher_kotlin/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.french_teacher_kotlin
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.MediaController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun SayTheColor(view: View){
+        val clickedButton : Button = view as Button
+        var mediaPlayer : MediaPlayer = MediaPlayer.create(this, resources.getIdentifier(clickedButton.tag.toString(), "raw" , packageName))
+        mediaPlayer.start()
     }
 }
